b7689bfde0ac2514425d8246f12ac05279038ed6
Andreea Ionescu <ionescu2609@gmail.com>
2021-04-13 16:47:30 +0300

    Sidebar menu completed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -72,3 +72,13 @@ for (i = 0; i < dropdown.length; i++) {
     }
   });
 }
+
+//Price Sidebar Value
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
